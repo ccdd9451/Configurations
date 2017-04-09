142cb292530fbb61ffeec35d7f0ea0c4ba8901a0
Kaidong Hu <dongforregister@gmail.com>
2017-04-09 10:53:09 -0400

    install modification

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-shpos=`dirname $0`
+shpos="$( cd "$( dirname "$0" )" && pwd )"
+echo $shpos
 mkdir -p ~/.config
 
 ln -s $shpos ~/.config/myconfig
+echo 'source ~/.config/myconfig/shellconfig/shellrc' >> ~/.zshrc
