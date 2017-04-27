07eff99d4c202c9fb66104eb20f1de4cdd503b48
Kaidong Hu <dongforregister@gmail.com>
2017-04-27 16:16:26 -0400

    update on write funcs

--- pymodule/xilio.py
+++ pymodule/xilio.py
@@ -1,3 +1,5 @@
+from os import chmod
+
 def load(path, mode='rb'):
     with open (path, mode) as file:
         from pickle import load
@@ -16,9 +18,11 @@ def readline(path, mode='r'):
     with open (path, mode) as file:
         yield from file.readline()
 
-def write(path, content, mode='w'):
+def write(path, content, mode='w', executable=False):
     with open (path, mode) as file:
         file.write(content)
+    if executable:
+        chmod(path, 0o755)
 
 def append(path, content, mode='a'):
     with open (path, mode) as file:
