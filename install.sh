6b8cac6bf99527d6b90fcdf08e1dd1ab7b2925fb
Kaidong Hu <dongforregister@gmail.com>
2017-05-09 18:03:07 -0400

    ctags config code

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ ln -s $shpos ~/.config/myconfig
 export CONFIGPATH=$HOME/.config/myconfig
 ln -s $CONFIGPATH/misc/gitconfig ~/.gitconfig
 ln -s $CONFIGPATH/misc/ssh_config ~/.ssh/config
+ln -s $CONFIGPATH/misc/ctags ~/.ctags
 
 
 echo "source $CONFIGPATH/shellconfig/shellrc" >> ~/.zshrc
