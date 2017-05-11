9dacd9ef00764953e4724868656f9504dce6f687
Kaidong Hu <dongforregister@gmail.com>
2017-05-10 20:27:15 -0400

    remove fzf (can not be invoked)

--- shell/keybinding.zsh
+++ shell/keybinding.zsh
@@ -4,9 +4,3 @@ bindkey '^L' vi-forward-blank-word
 bindkey '^D' kill-blank-word
 bindkey '^U' undo
 
-bindkey '^Ih' fzf-history-widget
-bindkey '^If' fzf-file-widget
-bindkey '^Ic' fzf-cd-widget
-bindkey '^Ii' fzf-completion
-
-
