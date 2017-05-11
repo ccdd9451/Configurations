fba377874087b15414873127fa6281ede34a20b3
Kaidong Hu <dongforregister@gmail.com>
2017-05-10 20:00:43 -0400

    keybindings

---
+++ shell/keybinding.zsh
@@ -0,0 +1,12 @@
+bindkey '^V' vi-cmd-mode
+bindkey '^H' vi-backward-blank-word
+bindkey '^L' vi-forward-blank-word
+bindkey '^D' kill-blank-word
+bindkey '^U' undo
+
+bindkey '^Ih' fzf-history-widget
+bindkey '^If' fzf-file-widget
+bindkey '^Ic' fzf-cd-widget
+bindkey '^Ii' fzf-completion
+
+
