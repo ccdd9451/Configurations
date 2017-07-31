e5d99a70adab1bbe475383be9949d2030c2bf115
Kaidong Hu <dongforregister@gmail.com>
2017-07-30 22:11:56 -0400

    xilio update

--- python/pymodule/xilio.py
+++ python/pymodule/xilio.py
@@ -1,4 +1,26 @@
 from os import chmod
+import sys
+
+try:
+    import sh
+
+    def copy(s):
+        if sys.platform.startswith('linux'): # for Ubuntu
+            sh.xclip("-selection", "clipboard", _in=s, _bg=True)
+            sh.xclip("-selection", "primary", _in=s, _bg=True)
+        elif sys.platform == 'darwin':
+            sh.pbcopy(_in=s, _bg=True)
+
+    def paste(s):
+        if sys.platform.startswith('linux'): # for Ubuntu
+            return sh.xclip("-selection", "clipboard", "-o", _in=s)
+        elif sys.platform == 'darwin':
+            return sh.pbpaste()
+
+except ImportError:
+    pass
+
+
 
 def load(path, mode='rb'):
     with open (path, mode) as file:
