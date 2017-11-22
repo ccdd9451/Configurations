9577021b63180e095645c1f9fda7484df6da930b
Mr_Melovdy <dongforregister@gmail.com>
2017-11-22 10:28:30 -0500

    Update falias.zsh
    gi

--- shell/falias.zsh
+++ shell/falias.zsh
@@ -20,22 +20,5 @@ fi
 # open_command
 alias open='open_command'
 
-# if [ `uname` = 'Darwin' ]
-# then
-#     alias rm='mv --backup=t -t /tmp '
-# fi
-
-# Depercated
-# alias init_try(){
-# cwd=`pwd`
-# while [ `pwd` != '\' ] 
-# do
-#     if [ -f 'README.ini' ]
-#     then
-# 	source README.ini; cat README.ini
-# 	break
-#     fi
-# done
-# cd $cwd
-# }
+function gi() { curl -L -s https://www.gitignore.io/api/\$@ ;}
 
